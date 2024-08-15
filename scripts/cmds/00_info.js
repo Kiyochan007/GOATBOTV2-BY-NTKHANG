6f2e7469cdc1b561c149de8bb82e5b3e8586fe11
Kiyochan007 <alwaysonextuber@gmail.com>
2024-08-15 11:54:36 +0545

    Update 00_info.js

--- scripts/cmds/00_info.js
+++ scripts/cmds/00_info.js
@@ -15,14 +15,11 @@ module.exports = {
 		envConfig: {}
 	},
 	onStart: async function ({ message }) {
-		const botName = "yazky";
-		const botPrefix = "!";
-		const authorName = "CliffVincent";
-		const ownAge = "18";
-		const teamName = "Github team";
-		const authorFB = "https://www.facebook.com/swordigo.swordslush";
-		const authorInsta = "Thead469";
-		const tikTok = "tiktok.com/@Cliffthegreat0";
+		const botName = "Yvette";
+		const botPrefix = ".";
+		const authorName = "Alwaysone";
+		const authorFB = "Not Available";
+		const authorInsta = "........";
 		const urls = JSON.parse(fs.readFileSync('cliff.json'));
 		const link = urls[Math.floor(Math.random() * urls.length)];
 		const now = moment().tz('Asia/Jakarta');
